4d10f36f3811460416a592f98cb98c37bdebb703
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-04 19:47:20 +0200

    сделал 2 задачи

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const divRefs = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join("");
+
+divRefs.insertAdjacentHTML("afterbegin", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
